87a560470c8d313cb45fb6f6282bf8679193e5bb
a b <marcel530@gmail.com>
2024-02-18 18:29:53 +0100

    Changed currency to USD

--- src/app/utils/formatMoney.ts
+++ src/app/utils/formatMoney.ts
@@ -1,6 +1,6 @@
 export const formatMoney = (amount: number) => {
-	return new Intl.NumberFormat("pl-PL", {
+	return new Intl.NumberFormat("en-US", {
 		style: "currency",
-		currency: "PLN",
+		currency: "USD",
 	}).format(amount);
 };
