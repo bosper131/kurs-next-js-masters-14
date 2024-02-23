3307367713a470e3e1a9971f25d1ef71a4b4e6e6
a b <marcel530@gmail.com>
2024-02-24 00:23:08 +0100

    drugi modul - dodanie home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function HomePage() {
-	return <div>Home page</div>;
+import { ProductList } from "@/app/ui/organisms/ProductList";
+import type { ProductsType } from "@/app/ui/organisms/ProductList.type";
+
+export default async function HomePage() {
+	const res = await fetch(
+		`https://naszsklep-api.vercel.app/api/products`,
+	);
+	const allProducts = (await res.json()) as ProductsType[];
+
+	return (
+		<>
+			<ProductList products={allProducts} />
+		</>
+	);
 }
