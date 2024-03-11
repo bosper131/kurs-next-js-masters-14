f800c020a937c70be07799452285a3f1599a0e9e
a b <marcel530@gmail.com>
2024-03-11 23:23:43 +0100

    fixes 5

--- src/app/ui/molecules/SearchInput.tsx
+++ src/app/ui/molecules/SearchInput.tsx
@@ -1,36 +1,31 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const SearchInput = () => {
 	const router = useRouter();
 	const [route, setRoute] = useState<string>();
-	const handleSubmit = (e: React.SyntheticEvent) => {
+	const [timer, setTimer] = useState(null);
+	const inputChanged = (e: React.SyntheticEvent) => {
 		e.preventDefault();
-		router.push(`/search?query=${route}`);
-		setRoute("");
+		setRoute(e.target.value);
+		clearTimeout(timer);
+
+		const newTimer = setTimeout(() => {
+			router.push(`/search?query=${route}`);
+			setRoute("");
+		}, 500);
+		setTimer(newTimer);
 	};
 
-	useEffect(() => {
-		const delayDebounceFn = setTimeout(() => {
-			router.push(`/search?query=${route}`);
-		}, 500);
-
-		return () => clearTimeout(delayDebounceFn);
-	}, [route]);
-
 	return (
-		<form onSubmit={handleSubmit} className="relative">
-			<input
-				type="text"
-				placeholder="Search"
-				name="route"
-				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-					setRoute(e.target.value);
-				}}
-				value={route}
-				className="h-10 w-full rounded-lg border pl-3 pr-10 text-sm text-gray-600 placeholder-gray-400 focus:border-blue-300 focus:outline-none focus:ring"
-			/>
-		</form>
+		<input
+			type="text"
+			placeholder="Search"
+			name="route"
+			onChange={inputChanged}
+			value={route}
+			className="h-10 w-full rounded-lg border pl-3 pr-10 text-sm text-gray-600 placeholder-gray-400 focus:border-blue-300 focus:outline-none focus:ring"
+		/>
 	);
 };
