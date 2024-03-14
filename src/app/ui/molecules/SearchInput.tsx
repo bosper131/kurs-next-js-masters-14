af77baced32e5c0741c53d6f6a838eddf9d09d5a
a b <marcel530@gmail.com>
2024-03-14 21:56:45 +0100

    Improving SearchInput

--- src/app/ui/molecules/SearchInput.tsx
+++ src/app/ui/molecules/SearchInput.tsx
@@ -4,19 +4,16 @@ import { useState } from "react";
 
 export const SearchInput = () => {
 	const router = useRouter();
-	const [route, setRoute] = useState<string>();
 	const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
 	const inputChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
 		e.preventDefault();
 		const target = e.target as HTMLInputElement;
-		setRoute(target.value);
 		if (timer !== null) {
 			clearTimeout(timer);
 		}
 
 		const newTimer = setTimeout(() => {
-			router.push(`/search?query=${route}`);
-			setRoute("");
+			router.push(`/search?query=${target.value}`);
 		}, 500);
 		setTimer(newTimer);
 	};
@@ -27,7 +24,6 @@ export const SearchInput = () => {
 			placeholder="Search"
 			name="route"
 			onChange={inputChanged}
-			value={route}
 			className="h-10 w-full rounded-lg border pl-3 pr-10 text-sm text-gray-600 placeholder-gray-400 focus:border-blue-300 focus:outline-none focus:ring"
 		/>
 	);
